937ae2b5c3a19e52affe21c9774d8ec9ef59737c
Willi0te <william.sinclair92@gmail.com>
2024-05-23 20:01:23 +0200

    adding env to stripe webhook

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -2,9 +2,15 @@ import { mongooseConnect } from "@/lib/mongoose";
 import { buffer } from "micro";
 const stripe = require("stripe")(process.env.STRIPE_SECRET);
 import { Order } from "@/models/Order";
+require("dotenv").config();
 
-const endpointSecret =
-    "whsec_2c323ddb48369c6610d7294e70e680d8cd3ab467c43b085792dce1fd590ed835";
+// for local testing
+// stripe CLI KEY: humor-favour-excite-agile
+//stripe account-ID: acct_1Ovzrp083Ddl5GGm
+// const endpointSecret =
+//     "whsec_2c323ddb48369c6610d7294e70e680d8cd3ab467c43b085792dce1fd590ed835";
+
+const endpointSecret = process.env.STRIPE_SECRET;
 
 const handler = async (req, res) => {
     await mongooseConnect();
@@ -47,6 +53,3 @@ const handler = async (req, res) => {
 export default handler;
 
 export const config = { api: { bodyParser: false } };
-
-// stripe CLI KEY: humor-favour-excite-agile
-//stripe account-ID: acct_1Ovzrp083Ddl5GGm
